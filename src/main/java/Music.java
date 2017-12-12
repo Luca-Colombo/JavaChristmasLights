e2eeeb8acc0c157802a1e42153999f0550a9923c
Luca-Colombo <lucacolo94@gmail.com>
2017-12-12 19:44:03 +0100

    Added the executable main class in Xmas

--- src/main/java/Music.java
+++ src/main/java/Music.java
@@ -3,23 +3,24 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- * 
+ *
  * @author Colombo Luca
  */
 
 
-public class Music implements Runnable{
+public class Music {
     static AudioInputStream audioStream;
     static File audioFile;
     static Clip clip;
 
-    private void openFile (String path) {
+
+    private static void openFile (String path) {
 
         audioFile = new File(path); //TODO insert pathname
 
     }
 
-    public void loadSong(String path) {
+    private static void loadSong(String path) {
 
         try {
 
@@ -33,36 +34,10 @@ public class Music implements Runnable{
         }
     }
 
-    private void play(){
+    public static void play(String path){
+        loadSong(path);
         clip.start();
-        System.out.println("Canzone partita");
-    }
-
-    public static void main(String[] args) {
-        Thread t = new Thread(new Music());
-        t.start();
 
     }
 
-    /**
-     * When an object implementing interface <code>Runnable</code> is used
-     * to create a thread, starting the thread causes the object's
-     * <code>run</code> method to be called in that separately executing
-     * thread.
-     * <p>
-     * The general contract of the method <code>run</code> is that it may
-     * take any action whatsoever.
-     *
-     * @see Thread#run()
-     */
-    @Override
-    public void run() {
-        loadSong("jinglebells.wav");
-        play();
-        try {
-            Thread.sleep(clip.getMicrosecondLength() / 1000);
-        } catch (InterruptedException e){
-            e.printStackTrace();
-        }
-    }
 }
--- src/main/java/Xmas.java
+++ src/main/java/Xmas.java
@@ -1,8 +1,60 @@
+import java.io.IOException;
+import java.util.List;
+
+import static java.lang.Thread.sleep;
+
 /**
  *
  * @author Colombo Luca
  */
 public class Xmas {
+
+    // The resolution at which the song is played
+    private final static int resolution = 100;
+
+    public static void main(String[] args) {
+        Lights lights = new Lights();
+        Music music = new Music();
+        List<Line> sequence;
+        long startTime;
+        long currentTime;
+
+
+        try {
+            sequence = CsvParser.parseType();
+
+            startTime = System.currentTimeMillis();
+
+            //TODO add path
+            music.play("diocibenedira.wav");
+
+            for (Line line : sequence) {
+
+                currentTime = System.currentTimeMillis() - startTime;
+
+                if (line.getTime() <= currentTime) {
+
+                    for (int ch = 0; ch <6; ch++) {
+                        if (line.getChannels()[ch] == 255) {
+                            lights.setHigh(ch);
+                        } else {
+                            lights.setLow(ch);
+
+                        }
+
+                    }
+                }
+                sleep(resolution);
+            }
+
+            lights.shutdown();
+
+
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
