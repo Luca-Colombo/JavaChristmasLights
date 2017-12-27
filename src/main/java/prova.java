7c1b2446c2829aeba9d3d6d1ecc7bbb4f3be628a
Luca-Colombo <lucacolo94@gmail.com>
2017-12-27 02:19:26 +0100

    implemented a kinda working HTTP Server

--- src/main/java/prova.java
+++ src/main/java/prova.java
@@ -11,6 +11,12 @@ import java.util.logging.Logger;
 
 public class prova extends NanoHTTPD {
 
+    boolean isOn = false;
+
+    testmain silleninni;
+
+    Thread t1;
+
     /**
      * logger to log to.
      */
@@ -30,15 +36,64 @@ public class prova extends NanoHTTPD {
         String uri = session.getUri();
         prova.LOG.info(method + " '" + uri + "' ");
 
-        String msg = "<html><body><h1>Hello server</h1>\n";
+        String msg = "<html><body><h1>Hello server</h1>";
         Map<String, String> parms = session.getParms();
-        System.out.println(parms.get("username"));
+        System.out.println(parms.keySet());
+        System.out.println(parms.get("choice"));
 
-        if (parms.get("username") == null) {
-            msg += "<form action='?' method='get'>\n" + "  <p>Your name: <input type='text' name='username'></p>\n" +
-                    "</form>\n";
+
+
+        if (uri.equals("/") && !isOn) {
+
+            msg += "<form action='acceso' method='get'>" + "<input type='submit' name='choice' value='acceso'>" +
+                    "</form>";
+
+
+
+        } else if (uri.equals("/") && isOn) {
+            msg += "<form action='off' method='get'>" + "<input type='submit' name='choice' value='spegni'>" +
+                    "</form>";
+        } else if (parms.get("choice").equals("acceso")){
+
+            silleninni = new testmain();
+            t1 = new Thread(silleninni);
+            t1.start();
+            isOn = true;
+
+            System.out.println("funziona!!");
+            //msg += "<p> PER ora funziona </p>";
+
+
+
+            msg += "<a class='clickMe' href='..//'>Click me</a>" +
+                    "<style>" +
+                    ".clickMe {" +
+                    "border-bottom: 1px solid #777777;"+
+                "border-left: 1px solid #000000;"+
+                "border-right: 1px solid #333333;"+
+                "border-top: 1px solid #000000;"+
+                "color: #000000;"+
+                "display: block;"+
+                "height: 2.5em;"+
+                "padding: 0 1em;"+
+                "width: 5em;"+
+                "text-decoration: none;"+
+                    "}" +
+                    "</style>";
+
+            msg += "<form action='off' method='get'>" + "<input type='submit' name='choice' value='spegni'>" +
+                    "</form>";
+
+        } else if (parms.get("choice").equals("spegni")){
+            silleninni.stopRunning();
+            isOn = false;
+
+            msg += "<form action='acceso' method='get'>" + "<input type='submit' name='choice' value='acceso'>" +
+                    "</form>";
+
+
         } else {
-            msg += "<p>Hello, " + parms.get("username") + "!</p>";
+            msg += "<p>Qualcosa non va!</p>";
         }
 
         msg += "</body></html>\n";
@@ -46,5 +101,7 @@ public class prova extends NanoHTTPD {
 
         return newFixedLengthResponse(msg);
     }
+
+
 }
 
